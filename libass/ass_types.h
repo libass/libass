5f2dfaa377c4afbfc6e456e5cd0b41a8bb5a7ee0
Grigori Goronzy <greg@blackbox>
2009-08-06 13:17:21 +0200

    Use int for ScaledBorderAndShadow
    Convert type for consistency with other booleans/flags that also use
    int.

--- libass/ass_types.h
+++ libass/ass_types.h
@@ -112,7 +112,7 @@ typedef struct ass_track {
     int PlayResY;
     double Timer;
     int WrapStyle;
-    char ScaledBorderAndShadow;
+    int ScaledBorderAndShadow;
 
     int default_style;      // index of default style
     char *name;             // file name in case of external subs, 0 for streams
