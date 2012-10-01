b61d260b3e92a3fc4c0e054ad5b030182030f996
Grigori Goronzy <greg@blackbox>
2012-10-01 20:08:12 +0200

    drawing: scale pbo according to drawing scale
    Fixes issue #68.

--- libass/ass_drawing.c
+++ libass/ass_drawing.c
@@ -87,6 +87,7 @@ static void drawing_prepare(ASS_Drawing *drawing)
 static void drawing_finish(ASS_Drawing *drawing, int raw_mode)
 {
     int i, offset;
+    double pbo;
     FT_BBox bbox = drawing->cbox;
     FT_Outline *ol = &drawing->outline;
 
@@ -103,11 +104,12 @@ static void drawing_finish(ASS_Drawing *drawing, int raw_mode)
 
     drawing->advance.x = bbox.xMax - bbox.xMin;
 
-    drawing->desc = double_to_d6(-drawing->pbo * drawing->scale_y);
+    pbo = drawing->pbo / (64.0 / (1 << (drawing->scale - 1)));
+    drawing->desc = double_to_d6(-pbo * drawing->scale_y);
     drawing->asc = bbox.yMax - bbox.yMin + drawing->desc;
 
     // Place it onto the baseline
-    offset = (bbox.yMax - bbox.yMin) + double_to_d6(-drawing->pbo *
+    offset = (bbox.yMax - bbox.yMin) + double_to_d6(-pbo *
                                                     drawing->scale_y);
     for (i = 0; i < ol->n_points; i++)
         ol->points[i].y += offset;
