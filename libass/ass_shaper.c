4a7d3878d1b1e45776de60d68825ca08b8725a4c
Grigori Goronzy <greg@blackbox>
2012-10-15 16:45:23 +0200

    Revert "shaper: add UCDN support"
    This reverts commit 840ce4c2d9381e340c3239d104933fea85fff3e5.
    
    Harfbuzz added UCDN support much quicker than I thought and the latest
    release 0.9.5 contains UCDN already, so we don't need it in libass
    anymore.

--- libass/ass_shaper.c
+++ libass/ass_shaper.c
@@ -29,9 +29,6 @@
 #define MAX_RUNS 50
 
 #ifdef CONFIG_HARFBUZZ
-#ifdef CONFIG_UCDN
-#include "hb-ucdn.h"
-#endif
 #include <hb-ft.h>
 enum {
     VERT = 0,
@@ -60,7 +57,6 @@ struct ass_shaper {
 
     // Glyph metrics cache, to speed up shaping
     Cache *metrics_cache;
-    hb_unicode_funcs_t *unicode_funcs;
 #endif
 };
 
@@ -112,9 +108,6 @@ void ass_shaper_free(ASS_Shaper *shaper)
 {
 #ifdef CONFIG_HARFBUZZ
     ass_cache_done(shaper->metrics_cache);
-#ifdef CONFIG_UCDN
-    hb_unicode_funcs_destroy(shaper->unicode_funcs);
-#endif
     free(shaper->features);
 #endif
     free(shaper->event_text);
@@ -442,9 +435,6 @@ static void shape_harfbuzz(ASS_Shaper *shaper, GlyphInfo *glyphs, size_t len)
         runs[run].buf    = hb_buffer_create();
         runs[run].font   = get_hb_font(shaper, glyphs + k);
         set_run_features(shaper, glyphs + k);
-#ifdef CONFIG_UCDN
-        hb_buffer_set_unicode_funcs(runs[run].buf, shaper->unicode_funcs);
-#endif
         hb_buffer_pre_allocate(runs[run].buf, i - k + 1);
         hb_buffer_set_direction(runs[run].buf, direction ? HB_DIRECTION_RTL :
                 HB_DIRECTION_LTR);
@@ -684,9 +674,6 @@ ASS_Shaper *ass_shaper_new(size_t prealloc)
 #ifdef CONFIG_HARFBUZZ
     init_features(shaper);
     shaper->metrics_cache = ass_glyph_metrics_cache_create();
-#ifdef CONFIG_UCDN
-    shaper->unicode_funcs = hb_ucdn_make_unicode_funcs();
-#endif
 #endif
 
     return shaper;
