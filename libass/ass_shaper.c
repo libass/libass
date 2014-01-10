ad15b5d4a986e106c6fc5149ba60cc2c7c4fc266
11rcombs <rodger.combs@gmail.com>
2014-01-09 18:41:06 -0600

    Adjustment to remove an awkward cast

--- libass/ass_shaper.c
+++ libass/ass_shaper.c
@@ -621,13 +621,12 @@ static void shape_harfbuzz(ASS_Shaper *shaper, GlyphInfo *glyphs, size_t len)
 void ass_shaper_determine_script(ASS_Shaper *shaper, GlyphInfo *glyphs,
                                   size_t len)
 {
-    int i;
     int backwards_scan = 0;
     hb_unicode_funcs_t *ufuncs = hb_unicode_funcs_get_default();
     hb_script_t last_script = HB_SCRIPT_UNKNOWN;
 
     // determine script (forward scan)
-    for (i = 0; i < (int)len; i++) {
+    for (unsigned i = 0; i < len; i++) {
         GlyphInfo *info = glyphs + i;
         info->script = hb_unicode_script(ufuncs, info->symbol);
 
@@ -648,7 +647,7 @@ void ass_shaper_determine_script(ASS_Shaper *shaper, GlyphInfo *glyphs,
 
     // determine script (backwards scan, if needed)
     last_script = HB_SCRIPT_UNKNOWN;
-    for (i = len - 1; i >= 0 && backwards_scan; i--) {
+    for (int i = len - 1; i >= 0 && backwards_scan; i--) {
         GlyphInfo *info = glyphs + i;
 
         // common/inherit codepoints inherit script from context
