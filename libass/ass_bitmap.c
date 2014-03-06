libass/libass

commit 04b51c2d70efda779adff96b4eea82c46682565b

Don't crash on \fscx0
Freetype can return a bounding box with all fields set to INT_MIN if an
outline with all points set to 0 is used. This can happen e.g. with
\fscx0, but also in more complicated cases. (In the original crashing
sample, this was probably caused in combination with an embedded font.)

Such a bounding box causes libass to crash, because it will enlarge the
combined bitmap bounding box to a ridiculous size.

Just skip outlines that have en empty bounding box. This is probably
the correct thing to do, and won't pass INT_MAX down to other parts
of libass.

libass/ass_bitmap.c
@@ -166,6 +166,9 @@ Bitmap *outline_to_bitmap(ASS_Library *library, FT_Library ftlib,
     FT_Bitmap bitmap;
 
     FT_Outline_Get_CBox(outline, &bbox);
+    if (bbox.xMin == bbox.xMax || bbox.yMin == bbox.yMax)
+        return NULL;
+
     // move glyph to origin (0, 0)
     bbox.xMin &= ~63;
     bbox.yMin &= ~63;

commit cc6fcf71627c39e932b0107fde86602be38c4dd8

Better fix for zero area outlines
Signed-off-by: wm4 <wm4@nowhere>

Commit 04b51c2d broke BorderStyle=3 with text that had spaces.

libass/ass_bitmap.c
@@ -166,8 +166,14 @@ Bitmap *outline_to_bitmap(ASS_Library *library, FT_Library ftlib,
     FT_Bitmap bitmap;
 
     FT_Outline_Get_CBox(outline, &bbox);
-    if (bbox.xMin == bbox.xMax || bbox.yMin == bbox.yMax)
-        return NULL;
+    if (bbox.xMin >= bbox.xMax || bbox.yMin >= bbox.yMax) {
+        Bitmap *bm = malloc(sizeof(Bitmap));
+        bm->buffer = NULL;
+        bm->w = bm->h = 0;
+        bm->stride = 0;
+        bm->left = bm->top = 0;
+        return bm;
+    }
 
     // move glyph to origin (0, 0)
     bbox.xMin &= ~63;