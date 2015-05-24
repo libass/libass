8c8741fe2000d4b4d89a53f894363a42288cec3e
Oleg Oshmyan <chortos@inbox.lv>
2015-05-25 01:00:38 +0300

    string2timecode: don't truncate to int
    The timecode is a long long, but it is computed as a product whose
    all multiplicands are (unsigned) ints and so effectively has the value
    of an (unsigned) int. Fix this, and use the full long long range,
    by explicitly making one of the first two multiplicands a long long.
    
    Found by Coverity Scan.

--- libass/ass.c
+++ libass/ass.c
@@ -188,7 +188,7 @@ static long long string2timecode(ASS_Library *library, char *p)
         ass_msg(library, MSGL_WARN, "Bad timestamp");
         return 0;
     }
-    tm = ((h * 60 + m) * 60 + s) * 1000 + ms * 10;
+    tm = ((h * 60LL + m) * 60 + s) * 1000 + ms * 10;
     return tm;
 }
 
