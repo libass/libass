libass/libass

commit d6bb9af645526a810512eaf2c65252ac0e0e6b36

fontselect: get rid of strdup for display name
The name is always pulled from the font info, which is static, so
there is no need to strdup.

libass/ass_fontselect.c
@@ -564,11 +564,11 @@ find_font(ASS_FontSelector *priv, ASS_Library *library,
             // used for display purposes so it doesn't matter that much,
             // though.
             if (selected->postscript_name)
-                result = strdup(selected->postscript_name);
+                result = selected->postscript_name;
             else
-                result = strdup(selected->families[0]);
+                result = selected->families[0];
         } else
-            result = strdup(selected->path);
+            result = selected->path;
     }
 
     return result;

libass/ass_font.c
@@ -153,7 +153,6 @@ static int add_face(ASS_FontSelector *fontsel, ASS_Font *font, uint32_t ch)
         if (font->faces_uid[i] == uid) {
             ass_msg(font->library, MSGL_INFO,
                     "Got a font face that already is available! Skipping.");
-            free(path);
             return i;
         }
     }
@@ -178,7 +177,6 @@ static int add_face(ASS_FontSelector *fontsel, ASS_Font *font, uint32_t ch)
         if (error) {
             ass_msg(font->library, MSGL_WARN,
                     "Error opening memory font: '%s'", path);
-            free(path);
             return -1;
         }
 
@@ -187,7 +185,6 @@ static int add_face(ASS_FontSelector *fontsel, ASS_Font *font, uint32_t ch)
         if (error) {
             ass_msg(font->library, MSGL_WARN,
                     "Error opening font: '%s', %d", path, index);
-            free(path);
             return -1;
         }
 
@@ -201,7 +198,6 @@ static int add_face(ASS_FontSelector *fontsel, ASS_Font *font, uint32_t ch)
                 if (error) {
                     ass_msg(font->library, MSGL_WARN,
                             "Error opening font: '%s', %d", path, i);
-                    free(path);
                     return -1;
                 }
 
@@ -217,7 +213,6 @@ static int add_face(ASS_FontSelector *fontsel, ASS_Font *font, uint32_t ch)
     font->faces[font->n_faces] = face;
     font->faces_uid[font->n_faces++] = uid;
     ass_face_set_size(face, font->size);
-    free(path);
     return font->n_faces - 1;
 }
 

commit a0ca3965fe8dd533578a2a918501aae758d17300

fontselect: fix memory leak
Introduced by commit d6bb9af6. Found by coverity scan.

libass/ass_fontselect.c
@@ -682,7 +682,7 @@ char *ass_font_select(ASS_FontSelector *priv, ASS_Library *library,
     }
 
     if (!res && priv->path_default) {
-        res = strdup(priv->path_default);
+        res = priv->path_default;
         *index = priv->index_default;
         ass_msg(library, MSGL_WARN, "fontselect: Using default font: "
                 "(%s, %d, %d) -> %s, %d, %s", family, bold, italic,