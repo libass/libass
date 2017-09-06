f97e0f457c21a65a924a541389a9375d0dc70f52
Rodger Combs <rodger.combs@gmail.com>
2017-09-05 21:15:40 -0500

    ass_set_fonts: empty cache as well

--- libass/ass_render_api.c
+++ libass/ass_render_api.c
@@ -146,6 +146,10 @@ void ass_set_fonts(ASS_Renderer *priv, const char *default_font,
 
     ass_reconfigure(priv);
 
+    ass_cache_empty(priv->cache.font_cache);
+    if (priv->shaper)
+        ass_shaper_empty_cache(priv->shaper);
+
     if (priv->fontselect)
         ass_fontselect_free(priv->fontselect);
     priv->fontselect = ass_fontselect_init(priv->library, priv->ftlibrary,
--- libass/ass_shaper.h
+++ libass/ass_shaper.h
@@ -27,6 +27,7 @@ typedef struct ass_shaper ASS_Shaper;
 void ass_shaper_info(ASS_Library *lib);
 ASS_Shaper *ass_shaper_new(size_t prealloc);
 void ass_shaper_free(ASS_Shaper *shaper);
+void ass_shaper_empty_cache(ASS_Shaper *shaper);
 void ass_shaper_set_kerning(ASS_Shaper *shaper, int kern);
 void ass_shaper_find_runs(ASS_Shaper *shaper, ASS_Renderer *render_priv,
                           GlyphInfo *glyphs, size_t len);
