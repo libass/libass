e2e276e0aff2ed138bf868d11260816da9072493
Jesse Chan <jc@linux.com>
2019-02-07 18:34:25 -0800

    libass: incorporate MSVC build support

---
+++ msvc/config.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NONSTDC_NO_DEPRECATE
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+#define inline __inline
+
+#define CONFIG_DIRECTWRITE	1
+#define CONFIG_RASTERIZER	1
+#define CONFIG_ASM			1
+#define CONFIG_HARFBUZZ     1
+#define CONFIG_WINUTF16     1
+
+#ifdef _WIN64
+#define __x86_64__
+#else
+#define __i386__
+#endif
---
+++ msvc/strings.h
@@ -0,0 +1,6 @@
+﻿#pragma once
+
+#include <string.h>
+
+#define strncasecmp _strnicmp
+#define strcasecmp _stricmp
