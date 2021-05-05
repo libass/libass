11607d56bd64c54f5a7bbdc631d96b0bb29fc8a4
rcombs <rcombs@rcombs.me>
2021-05-05 01:50:32 -0500

    ass_utils: add format-string attr to ass_msg
    Allows compilers to print warnings when the args we pass don't match the string

--- libass/ass_utils.h
+++ libass/ass_utils.h
@@ -104,6 +104,9 @@ int numpad2align(int val);
 unsigned ass_utf8_get_char(char **str);
 unsigned ass_utf8_put_char(char *dest, uint32_t ch);
 void ass_utf16be_to_utf8(char *dst, size_t dst_size, uint8_t *src, size_t src_size);
+#ifdef __GNUC__
+    __attribute__ ((format (printf, 3, 4)))
+#endif
 void ass_msg(ASS_Library *priv, int lvl, const char *fmt, ...);
 int lookup_style(ASS_Track *track, char *name);
 ASS_Style *lookup_style_strict(ASS_Track *track, char *name, size_t len);
