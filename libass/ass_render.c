130e616072a21dd0abb6eec4ff1814a3738b47d2
passerbya <hanghang3103@163.com>
2021-09-11 16:07:56 +0800

    Update ass_render.c
    FIX: ScaledBorderAndShadow=no not working

--- libass/ass_render.c
+++ libass/ass_render.c
@@ -979,7 +979,9 @@ static void init_font_scale(ASS_Renderer *render_priv)
         render_priv->border_scale =
             font_scr_h / render_priv->track->PlayResY;
     else
-        render_priv->border_scale = render_priv->blur_scale;
+        render_priv->border_scale = 1;
+    if (!settings_priv->storage_height)
+        render_priv->blur_scale = render_priv->border_scale;
 
     if (render_priv->state.apply_font_scale) {
         render_priv->font_scale *= settings_priv->font_size_coeff;
