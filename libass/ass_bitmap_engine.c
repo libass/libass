99017a5bb0e88a3ad7100ecedb63c80475f5bbca
Dr.Smile <vabnick@gmail.com>
2022-08-09 03:58:25 +0300

    Merge ass_func_template.h into ass_bitmap_engine.c

--- libass/ass_bitmap_engine.c
+++ libass/ass_bitmap_engine.c
@@ -21,24 +21,81 @@
 
 #include "ass_bitmap_engine.h"
 
-#define ALIGN           C_ALIGN_ORDER
-#define DECORATE(func)  ass_##func##_c
-#include "ass_func_template.h"
-#undef ALIGN
-#undef DECORATE
+#define RASTERIZER_PROTOTYPES(tile_size, suffix) \
+    FillSolidTileFunc     ass_fill_solid_tile     ## tile_size ## _ ## suffix; \
+    FillHalfplaneTileFunc ass_fill_halfplane_tile ## tile_size ## _ ## suffix; \
+    FillGenericTileFunc   ass_fill_generic_tile   ## tile_size ## _ ## suffix; \
+    MergeTileFunc         ass_merge_tile          ## tile_size ## _ ## suffix;
+
+#define PARAM_BLUR_SET(suffix) \
+    ass_blur4_ ## suffix, \
+    ass_blur5_ ## suffix, \
+    ass_blur6_ ## suffix, \
+    ass_blur7_ ## suffix, \
+    ass_blur8_ ## suffix
+
+#define GENERIC_PROTOTYPES(suffix) \
+    BitmapBlendFunc ass_add_bitmaps_  ## suffix; \
+    BitmapBlendFunc ass_imul_bitmaps_ ## suffix; \
+    BitmapMulFunc   ass_mul_bitmaps_  ## suffix; \
+    BeBlurFunc      ass_be_blur_      ## suffix; \
+    Convert8to16Func ass_stripe_unpack_ ## suffix; \
+    Convert16to8Func ass_stripe_pack_   ## suffix; \
+    FilterFunc ass_shrink_horz_ ## suffix, ass_shrink_vert_ ## suffix; \
+    FilterFunc ass_expand_horz_ ## suffix, ass_expand_vert_ ## suffix; \
+    ParamFilterFunc PARAM_BLUR_SET(horz_ ## suffix); \
+    ParamFilterFunc PARAM_BLUR_SET(vert_ ## suffix);
+
+#define BITMAP_ENGINE(align_order_, tile_order_, tile_size, suffix) \
+    const BitmapEngine ass_bitmap_engine_ ## suffix = { \
+        .align_order    = align_order_, \
+        .tile_order     = tile_order_, \
+        .fill_solid     = ass_fill_solid_tile     ## tile_size ## _ ## suffix, \
+        .fill_halfplane = ass_fill_halfplane_tile ## tile_size ## _ ## suffix, \
+        .fill_generic   = ass_fill_generic_tile   ## tile_size ## _ ## suffix, \
+        .merge_tile     = ass_merge_tile          ## tile_size ## _ ## suffix, \
+        .add_bitmaps    = ass_add_bitmaps_  ## suffix, \
+        .imul_bitmaps   = ass_imul_bitmaps_ ## suffix, \
+        .mul_bitmaps    = ass_mul_bitmaps_  ## suffix, \
+        .be_blur        = ass_be_blur_      ## suffix, \
+        .stripe_unpack  = ass_stripe_unpack_ ## suffix, \
+        .stripe_pack    = ass_stripe_pack_   ## suffix, \
+        .shrink_horz    = ass_shrink_horz_   ## suffix, \
+        .shrink_vert    = ass_shrink_vert_   ## suffix, \
+        .expand_horz    = ass_expand_horz_   ## suffix, \
+        .expand_vert    = ass_expand_vert_   ## suffix, \
+        .blur_horz  = { PARAM_BLUR_SET(horz_ ## suffix) }, \
+        .blur_vert  = { PARAM_BLUR_SET(vert_ ## suffix) }, \
+    };
+
+
+GENERIC_PROTOTYPES(c)
+#if CONFIG_LARGE_TILES
+RASTERIZER_PROTOTYPES(32, c)
+BITMAP_ENGINE(C_ALIGN_ORDER, 5, 32, c)
+#else
+RASTERIZER_PROTOTYPES(16, c)
+BITMAP_ENGINE(C_ALIGN_ORDER, 4, 16, c)
+#endif
 
 #if CONFIG_ASM && ARCH_X86
 
-#define ALIGN           4
-#define DECORATE(func)  ass_##func##_sse2
-#include "ass_func_template.h"
-#undef ALIGN
-#undef DECORATE
+GENERIC_PROTOTYPES(sse2)
+#if CONFIG_LARGE_TILES
+RASTERIZER_PROTOTYPES(32, sse2)
+BITMAP_ENGINE(4, 5, 32, sse2)
+#else
+RASTERIZER_PROTOTYPES(16, sse2)
+BITMAP_ENGINE(4, 4, 16, sse2)
+#endif
 
-#define ALIGN           5
-#define DECORATE(func)  ass_##func##_avx2
-#include "ass_func_template.h"
-#undef ALIGN
-#undef DECORATE
+GENERIC_PROTOTYPES(avx2)
+#if CONFIG_LARGE_TILES
+RASTERIZER_PROTOTYPES(32, avx2)
+BITMAP_ENGINE(5, 5, 32, avx2)
+#else
+RASTERIZER_PROTOTYPES(16, avx2)
+BITMAP_ENGINE(5, 4, 16, avx2)
+#endif
 
 #endif
