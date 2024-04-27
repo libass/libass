b38eed68b316279b38a07ba5d8fbca5dd4008793
Jan Engelhardt <jengelh@inai.de>
2024-04-27 12:28:45 +0200

    build: add ELF symbol versions
    Building ffmpeg with libass-0.17.0 only encodes "libass.so.9" into
    ffmpeg. This is a problem when the ffmpeg binary is then taken to run
    on a system with just libass-0.14.0 (unknown symbol
    ass_track_set_feature).
    
    Adding ELF symbol versions makes ld add extra information to ffmpeg
    that then e.g. rpm can use to specify additional dependencies and
    prevent installation of an unworkable combination. ld.so also emits a
    better error message that the wrong libass version is present.

--- gen_defs.py
+++ gen_defs.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 
 symfile_path = sys.argv[1]
 deffile_path = sys.argv[2]
 
-with open(symfile_path) as symfile:
-    lines = symfile.readlines()
-    lines.insert(0, 'EXPORTS\n')
-    with open(deffile_path, 'w') as deffile:
-        deffile.writelines(lines)
+with open(deffile_path, 'w') as deffile:
+    deffile.write("EXPORTS\n")
+    for line in open(symfile_path, 'r'):
+        a = re.search('(ass_\w+)', line)
+        if a is not None:
+            deffile.write(a.group(1) + "\n")
