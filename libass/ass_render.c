libass/libass

commit 12f3e45f117b53a59a25457ce3e36cff5738c495

Simplify & fix pre/post-fade alpha handling
Rather than apply \fade early and save pre-fade values,
just don't apply \fade until necessary.

In particular, this fixes split_style_runs during fades,
when it could fail to detect small alpha changes:
    {\fad(...)\2a0}f{\2a1}i
as the alphas get rounded to the same value after applying the fade.

libass/ass_render.c
@@ -2145,12 +2145,8 @@ static bool parse_events(RenderContext *state, ASS_Event *event)
         // Fill glyph information
         info->symbol = code;
         info->font = state->font;
-        for (int i = 0; i < 4; i++) {
-            uint32_t clr = state->c[i];
-            info->a_pre_fade[i] = _a(clr);
-            ass_apply_fade(&clr, state->fade);
-            info->c[i] = clr;
-        }
+        for (int i = 0; i < 4; i++)
+            info->c[i] = state->c[i];
 
         info->effect_type = state->effect_type;
         info->effect_timing = state->effect_timing;
@@ -2503,15 +2499,15 @@ static void render_and_combine_glyphs(RenderContext *state,
             if (flags & FILTER_NONZERO_SHADOW &&
                 (info->effect_type == EF_KARAOKE_KF ||
                  info->effect_type == EF_KARAOKE_KO ||
-                 (info->a_pre_fade[0]) != 0xFF ||
+                 _a(info->c[0]) != 0xFF ||
                  info->border_style == 3))
                 flags |= FILTER_FILL_IN_SHADOW;
             if (!(flags & FILTER_NONZERO_BORDER) &&
                 !(flags & FILTER_FILL_IN_SHADOW))
                 flags &= ~FILTER_NONZERO_SHADOW;
             if ((flags & FILTER_NONZERO_BORDER &&
-                 info->a_pre_fade[0] == 0 &&
-                 info->a_pre_fade[1] == 0 &&
+                 _a(info->c[0]) == 0 &&
+                 _a(info->c[1]) == 0 &&
                  info->fade == 0) ||
                 info->border_style == 3)
                 flags |= FILTER_FILL_IN_BORDER;
@@ -2528,6 +2524,8 @@ static void render_and_combine_glyphs(RenderContext *state,
                 current_info = &combined_info[nb_bitmaps];
 
                 memcpy(&current_info->c, &info->c, sizeof(info->c));
+                ass_apply_fade(current_info->c, info->fade);
+
                 current_info->effect_type = info->effect_type;
                 current_info->effect_timing = info->effect_timing;
                 current_info->leftmost_x = OUTLINE_MAX;

libass/ass_render.h
@@ -98,7 +98,7 @@ typedef enum {
 // describes a combined bitmap
 typedef struct {
     FilterDesc filter;
-    uint32_t c[4];              // colors
+    uint32_t c[4];              // colors (with fade applied)
     Effect effect_type;
 
     // during render_and_combine_glyphs: distance in subpixels from the karaoke origin.
@@ -143,7 +143,6 @@ typedef struct glyph_info {
     char linebreak;             // the first (leading) glyph of some line ?
     bool starts_new_run;
     uint32_t c[4];              // colors
-    uint8_t a_pre_fade[4];      // alpha values before applying fades
     ASS_Vector advance;         // 26.6
     ASS_Vector cluster_advance;
     Effect effect_type;

commit c8ccdfde7f9d7ad96b8fe10b03b20e3f550517f3

Fix the fix for pre/post-fade alpha handling
Actually apply the fade to all four colors.

Regression in commit 12f3e45f117b53a59a25457ce3e36cff5738c495.

Fixes: https://github.com/libass/libass/issues/895

libass/ass_render.c
@@ -2524,7 +2524,8 @@ static void render_and_combine_glyphs(RenderContext *state,
                 current_info = &combined_info[nb_bitmaps];
 
                 memcpy(&current_info->c, &info->c, sizeof(info->c));
-                ass_apply_fade(current_info->c, info->fade);
+                for (int i = 0; i < 4; i++)
+                    ass_apply_fade(&current_info->c[i], info->fade);
 
                 current_info->effect_type = info->effect_type;
                 current_info->effect_timing = info->effect_timing;