b26b6dc69d9e016c4d0c4308fe046b904a0f18e6
robxnano <89391914+robxnano@users.noreply.github.com>
2025-08-19 15:42:58 +0100

    meson: Add exported symbols list for Apple linker
    Allows for correct symbol exports on macOS using ld64.

--- gen_defs.py
+++ gen_defs.py
@@ -13,6 +13,16 @@ def gen_mapfile(symfile_path: str, mapfile_path: str):
         with open(mapfile_path, 'w') as mapfile:
             mapfile.writelines(output)
 
+# Exported symbols file, for Apple linker
+def gen_expsym(symfile_path: str, expsymfile_path: str):
+    with open(symfile_path) as symfile:
+        lines = symfile.readlines()
+        output = ''
+        for line in lines:
+            output += f'_{line.strip()}\n'
+        with open(expsymfile_path, 'w') as expsymfile:
+            expsymfile.writelines(output)
+
 # DEF file, for Microsoft link.exe
 def gen_deffile(symfile_path: str, deffile_path: str):
     with open(symfile_path) as symfile:
@@ -23,5 +33,7 @@ def gen_deffile(symfile_path: str, deffile_path: str):
 
 if sys.argv[1] == '--mapfile':
     gen_mapfile(sys.argv[2], sys.argv[3])
+elif sys.argv[1] == '--expsym':
+    gen_expsym(sys.argv[2], sys.argv[3])
 else:
     gen_deffile(sys.argv[1], sys.argv[2])
